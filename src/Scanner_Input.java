cbc213a154b34b5da92e2f25e7b395767538296c
anshsahu-01 <anshsahu178@gmail.com>
2026-06-26 13:24:45 +0530

    Learned To Take Input From User In Java

---
+++ src/Scanner_Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Scanner_Input {
+    public static void main(String[] args){
+        System.out.println("Enter the integer value : ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}

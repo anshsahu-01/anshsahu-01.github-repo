00396e799522b4e743358359e07d66d9a5d2b1ff
anshsahu-01 <anshsahu178@gmail.com>
2026-06-26 14:15:49 +0530

    Problem Taking Name & Age Input From User Solved

---
+++ src/Name_and_age.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Name_and_age {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Name :");
+        String name = sc.nextLine();
+        System.out.println("Enter Your Age :");
+        int age = sc.nextInt();
+        System.out.println("Hello Your Name Is " + name + " and your age is " + age);
+    }
+}

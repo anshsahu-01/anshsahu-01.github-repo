5b3aa97ff7798d253cd46dadc93788c6ade3b8b2
anshsahu-01 <anshsahu178@gmail.com>
2026-06-29 21:37:11 +0530

    Analysed Problem and Solved With Different Approach

--- src/Leap_Year.java
+++ src/Leap_Year.java
@@ -6,7 +6,7 @@ public class Leap_Year {
         System.out.println("Enter Year :");
         int year = sc.nextInt();
 
-        if(year%4 == 0){
+        if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
             System.out.println("This is a leap year");
         }else{
             System.out.println("This is not a leap year");

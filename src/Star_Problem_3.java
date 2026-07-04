5357e9b61b60b8605cb8fab8d22395a271ba808a
anshsahu-01 <anshsahu178@gmail.com>
2026-07-04 22:22:52 +0530

    Solved Star Problem 5

--- src/Star_Problem_3.java
+++ src/Star_Problem_3.java
@@ -7,7 +7,7 @@ public class Star_Problem_3 {
         System.out.println("Enter your number");
         int num = sc.nextInt();
 
-        for (int i = num; i >= 0; i--) {
+        for (int i = num; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
---
+++ src/Star_Problem_5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Star_Problem_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your number");
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j < 2*num ; j++) {
+                if (i+j == 2*num || i == j){
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}

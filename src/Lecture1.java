63cf0b1a1cd2aad8c2d22d1e09b32b3c929d81aa
anshsahu-01 <anshsahu178@gmail.com>
2026-07-05 13:58:43 +0530

    Rearranged Folder in Packages

--- src/Lecture1.java
+++
@@ -1,6 +0,0 @@
-public class Lecture1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println();
-    }
-}
--- src/Variables.java
+++
@@ -1,8 +0,0 @@
-public class Variables {
-    public static void main(String[] args){
-        int a = 2;
-        int b = 5;
-        int sum = a + b ;
-        System.out.println(sum);
-    }
-}

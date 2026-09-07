ffe208029d90bfcca96688d2a47241e1db12bd35
anshsahu-01 <anshsahu178@gmail.com>
2026-09-07 20:55:45 +0530

    Solved Left Shift By One Element Array Problem

---
+++ src/Arrays_Problems/Left_One_Rotate.java
@@ -0,0 +1,17 @@
+package Arrays_Problems;
+import java.util.Arrays;
+
+public class Left_One_Rotate {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int n = arr.length;
+
+        int temp = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            arr[i-1] = arr[i];
+        }
+        arr [n-1] = temp;
+        System.out.println(Arrays.toString(arr));
+    }
+}

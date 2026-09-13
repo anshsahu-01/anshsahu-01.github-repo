5887fc688236936fee935a7eb486e66aa19a551a
anshsahu-01 <anshsahu178@gmail.com>
2026-09-13 21:50:45 +0530

    Learned Multi Dimensional Array

---
+++ src/Multidimentional_Array/Initialization.java
@@ -0,0 +1,30 @@
+package Multidimentional_Array;
+
+import java.util.Scanner;
+
+public class Initialization {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of rows :");
+        int rows = sc.nextInt();
+        System.out.println("Enter number of cols :");
+        int cols = sc.nextInt();
+
+        int[][] arr = new int[rows][cols];
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println("Enter element of matrix [" + i + " ]" + "[" + j + " ]");
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

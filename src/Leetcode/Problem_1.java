b7fefe2387419c657697ae0b4327459b0e644231
anshsahu-01 <anshsahu178@gmail.com>
2026-09-13 22:33:27 +0530

    Solved LeetCode Problem No 1

---
+++ src/Leetcode/Problem_1.java
@@ -0,0 +1,16 @@
+package Leetcode;
+
+public class Problem_1 {
+    public int[] twoSum(int[] arr, int target){
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if(arr[i] + arr[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}

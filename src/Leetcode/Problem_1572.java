40067dae745a2d9f8d83583da9c178de6482528c
anshsahu-01 <anshsahu178@gmail.com>
2026-09-18 20:38:51 +0530

    Solved LeetCode Problem Transpose Matrix #867

--- src/Leetcode/Problem_1572.java
+++ src/Leetcode/Problem_1572.java
@@ -1,7 +1,15 @@
 package Leetcode;
 
 public class Problem_1572 {
-    public static void main(String[] args) {
-        
+    public int diagonalSum(int[][] arr) {
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length; j++){
+                if(i == j || i + j == arr.length - 1){
+                    sum += arr[i][j];
+                }
+            }
+        }
+        return sum;
     }
 }
---
+++ src/Leetcode/Problem_867.java
@@ -0,0 +1,22 @@
+package Leetcode;
+
+import java.util.Arrays;
+
+public class Problem_867 {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3},{4,5,6}};
+        int rows = arr.length;
+        int cols = arr[0].length;
+        int[][] ans = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ans[j][i] = arr[i][j];
+            }
+        }
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(Arrays.toString(ans[i]));
+        }
+
+    }
+}
